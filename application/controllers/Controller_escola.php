ea3f0007f6c17fe5f5a68d02b771c22c2e907a6c
Jeysson Moura <jeyssonmoura@gmail.com>
2019-09-25 20:42:18 -0300

    Adicionado função addEscola

--- application/controllers/Controller_escola.php
+++ application/controllers/Controller_escola.php
@@ -7,4 +7,19 @@ class Controller_escola extends CI_Controller{
         $this->load->view('corpo/escola_cad_corpo');
         $this->load->view('estrutura/rodapePage');
     }
+    
+    public function addEscola () {
+        $this->load->Model('Model_escola');
+        $dados = array (
+            'esc_descricao' => $this->input->post('esc_descricao'),
+            'esc_cep' => $this->input->post('esc_cep'),
+            'esc_cidade' => $this->input->post('esc_cidade'),
+            'esc_bairro' => $this->input->post('esc_bairro'),
+            'esc_rua' => $this->input->post('esc_rua'),
+            'esc_uf' => $this->input->post('esc_uf'),
+            'esc_numero' => $this->input->post('esc_numero')
+        );
+        $r = $this->Model_escola->insert($dados);
+        echo json_encode($r);
+    }
 }
